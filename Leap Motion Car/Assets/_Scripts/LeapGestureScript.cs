b452c1987d8e9c15149280986c1555d012b4956b
Miguel <miguel.arindaeng@gmail.com>
2015-04-01 14:31:03 -0400

    Add Unity Project

---
+++ Leap Motion Car/Assets/_Scripts/LeapGestureScript.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapGestureScript : MonoBehaviour {
+
+	Controller leapController;
+	public TextMesh screenText;
+	private string gestureText;
+
+	void Start() {
+		gestureText = "Welcome to your Car!";
+		SetScreenText();
+		leapController = new Controller();
+		leapController.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+		leapController.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+	}
+
+	void Update() {
+		Frame frame = leapController.Frame();
+
+		GestureList gestures = frame.Gestures();
+
+		for (int i = 0; i < frame.Gestures().Count; i++) {
+			Gesture currGest = gestures[i];
+
+			switch (currGest.Type) {
+
+				case Gesture.GestureType.TYPECIRCLE:
+					CircleGesture circle = new CircleGesture(currGest);
+					if (circle.Pointable.Direction.AngleTo(circle.Normal) <= Mathf.PI/2) {//&& currGest.State.ToString() == "STATE_END") { // finger clockwise
+						Debug.Log("CW");
+						gestureText = "Volume Up";
+						SetScreenText();
+					}
+					else { //if (currGest.State.ToString() == "STATE_END") { // finger counter-clockwise
+						Debug.Log("CCW");
+						gestureText = "Volume Down";
+						SetScreenText();
+					}
+					break;
+			
+				case Gesture.GestureType.TYPESWIPE:
+					SwipeGesture swipe = new SwipeGesture(currGest);
+					if (Mathf.Abs(swipe.Direction.x) > Mathf.Abs(swipe.Direction.y)) { // gesture is horizontal swipe
+						if (swipe.Direction.x > 0) { // right swipe
+							Debug.Log ("SWR");
+							gestureText = "Wipers Off";
+							SetScreenText();
+						}
+						else { // left swipe
+							Debug.Log ("SWL");
+							gestureText = "Wipers On";
+							SetScreenText();
+						}
+					}
+					else { // gesture is vertical swipe
+						if (swipe.Direction.y > 0) { // gesture is swipe down
+							Debug.Log("SWD");
+							gestureText = "Sunroof Closed";
+							SetScreenText();
+						}
+						else { // gesture is swipe up
+							Debug.Log("SWU");
+							gestureText = "Sunroof Open";
+							SetScreenText();
+						}
+					}
+				    break;
+			}
+
+		}
+	}
+	
+	void SetScreenText() {
+		screenText.text = gestureText;
+	}
+}
---
+++ Roll-A-Ball Leap/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	Controller leapController;
+
+	void Start() {
+		count = 0;
+		SetCountText();
+		winText.text = "";
+		leapController = new Controller();
+	}
+
+	void FixedUpdate() { // sphere movement with Leap
+		Frame frame = leapController.Frame ();
+		float moveHorizontal = Input.GetAxis("Horizontal"); //= 0.0f;
+		float moveVertical = Input.GetAxis("Vertical"); //= 0.0f; 
+
+		HandList hands = frame.Hands;
+		for (int i = 0; i < frame.Hands.Count; i++) {
+			Hand hand = hands[i];
+			Vector handSpeed = hand.PalmVelocity;
+
+			moveHorizontal = (handSpeed.x)/100.0f;
+			Debug.Log(moveHorizontal);
+			moveVertical = ((handSpeed.z)/100.0f)*(-1.0f);
+			Debug.Log(moveVertical);
+
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 10) {
+			winText.text = "You Win!";
+		}
+	}
+}
