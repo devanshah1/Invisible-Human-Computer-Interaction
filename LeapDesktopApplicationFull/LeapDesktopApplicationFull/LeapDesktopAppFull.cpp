c59364df3d01a83beff7255faec46dbcef5cae07
devanshah1 <devan.shah@uoit.net>
2015-04-09 00:28:44 -0400

    LeapDesktopApplicationFull - V3.0.0.0
    - Fully built and ready for submission
    - All comment out support has been enabled and everything rebuild

--- LeapDesktopApplicationFull/LeapDesktopApplicationFull/LeapDesktopAppFull.cpp
+++ LeapDesktopApplicationFull/LeapDesktopApplicationFull/LeapDesktopAppFull.cpp
@@ -192,10 +192,10 @@ void LeapDesktopAppFull::draw ()
     gl::clear ( Color ( 255, 255, 255 ) );
 
     // Enable the required Gestures
-    //leap.enableGesture ( Gesture::TYPE_CIRCLE );
-    //leap.enableGesture ( Gesture::TYPE_KEY_TAP );
-    //leap.enableGesture ( Gesture::TYPE_SCREEN_TAP );
-    //leap.enableGesture ( Gesture::TYPE_SWIPE );
+    leap.enableGesture ( Gesture::TYPE_CIRCLE );
+    leap.enableGesture ( Gesture::TYPE_KEY_TAP );
+    leap.enableGesture ( Gesture::TYPE_SCREEN_TAP );
+    leap.enableGesture ( Gesture::TYPE_SWIPE );
 
     // Set the policy flag to make sure that the application is able to listen for background frames
     leap.setPolicyFlags ( Leap::Controller::POLICY_BACKGROUND_FRAMES );
@@ -207,7 +207,7 @@ void LeapDesktopAppFull::draw ()
     determineHandAndPerformAction ( frame, leap );
 
     // Sort through the gestures and perform the necessary actions that are associated to the gestures.
-    //determineGestureAndPerformAction ( frame, leap );
+    determineGestureAndPerformAction ( frame, leap );
 
     // Get the data for the window 
     WindowData *data = getWindow ()->getUserData<WindowData> () ;
